cec90c654e189821ffdcd4852d95b4f7cf4cdffd
darrenak403 <ngothanhdat4002@gmail.com>
2025-10-06 13:08:31 +0700

    feat: add About section and refactor Hero component layout for improved structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Hero from "../sections/Hero";
+import About from "../sections/About";
+import Serrvices from "../sections/Serrvices";
+import Work from "../sections/Work";
+import Marquee from "../sections/Marquee";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <About />
+      <Serrvices />
+      <Work />
+      <Marquee />
     </>
   );
 };
---
+++ src/sections/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div className="h-screen bg-white rounded-tl-[60px] rounded-tr-[60px]">
+        <div className="about-text main-container py-4 lg:py-12 h-full flex justify-center items-center font-heading text-black text-2xl md:text-3xl xl:text-5xl leading-[1.25]">
+          My name is Thanh Dat, I'm a web developer and designer and a
+          third-year student at FPT University (as of this project). I have a
+          passion for creating beautiful and functional websites, with a strong
+          background in HTML, CSS, JavaScript, React, Next.js, and .NET. I am
+          always looking for new challenges and opportunities to learn and grow
+          as a developer and designer.
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
